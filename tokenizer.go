c44df067d95f1e0068d5f72bc292e4af555d0592
j178 <10510431+j178@users.noreply.github.com>
2024-01-17 18:03:31 +0800

    feat: improve model selection

--- tokenizer.go
+++ tokenizer.go
@@ -69,10 +69,11 @@ package tokenizer
 //		// this should print the original string back
 //		text, _ := enc.Decode(ids)
 //		fmt.Println(text)
-//}
+// }
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/tiktoken-go/tokenizer/codec"
 )
@@ -87,43 +88,6 @@ type Codec interface {
 	Encode(string) ([]uint, []string, error)
 	Decode([]uint) (string, error)
 }
-
-type Model string
-
-const (
-	GPT4                     Model = "gpt-4"
-	GPT35Turbo               Model = "gpt-3.5-turbo"
-	TextEmbeddingAda002      Model = "text-embedding-ada-002"
-	TextDavinci003           Model = "text-davinci-003"
-	TextDavinci002           Model = "text-davinci-002"
-	CodeDavinci002           Model = "code-davinci-002"
-	CodeDavinci001           Model = "code-davinci-001"
-	CodeCushman002           Model = "code-cushman-002"
-	CodeCushman001           Model = "code-cushman-001"
-	DavinciCodex             Model = "davinci-codex"
-	CushmanCodex             Model = "cushman-codex"
-	TextDavinci001           Model = "text-davinci-001"
-	TextCurie001             Model = "text-curie-001"
-	TextBabbage001           Model = "text-babbage-001"
-	TextAda001               Model = "text-ada-001"
-	Davinci                  Model = "davinci"
-	Curie                    Model = "curie"
-	Babbage                  Model = "babbage"
-	Ada                      Model = "ada"
-	TextSimilarityDavinci001 Model = "text-similarity-davinci-001"
-	TextSimilarityCurie001   Model = "text-similarity-curie-001"
-	TextSimilarityBabbage001 Model = "text-similarity-babbage-001"
-	TextSimilarityAda001     Model = "text-similarity-ada-001"
-	TextSearchDavinciDoc001  Model = "text-search-davinci-doc-001"
-	TextSearchCurieDoc001    Model = "text-search-curie-doc-001"
-	TextSearchAdaDoc001      Model = "text-search-ada-doc-001"
-	TextSearchBabbageDoc001  Model = "text-search-babbage-doc-001"
-	CodeSearchBabbageCode001 Model = "code-search-babbage-code-001"
-	CodeSearchAdaCode001     Model = "code-search-ada-code-001"
-	TextDavinciEdit001       Model = "text-davinci-edit-001"
-	CodeDavinciEdit001       Model = "code-davinci-edit-001"
-	GPT2                     Model = "gpt2"
-)
 
 type Encoding string
 
@@ -154,32 +118,88 @@ func Get(encoding Encoding) (Codec, error) {
 	}
 }
 
+// Reference: https://github.com/openai/tiktoken/blob/main/tiktoken/model.py
+
+var modelPrefixToEncoding = map[string]Encoding{
+	// chat
+	"gpt-4-":         "cl100k_base", // e.g., gpt-4-0314, etc., plus gpt-4-32k
+	"gpt-3.5-turbo-": "cl100k_base", // e.g, gpt-3.5-turbo-0301, -0401, etc.
+	"gpt-35-turbo-":  "cl100k_base", // Azure deployment name
+	// fine-tuned
+	"ft:gpt-4":         "cl100k_base",
+	"ft:gpt-3.5-turbo": "cl100k_base",
+	"ft:davinci-002":   "cl100k_base",
+	"ft:babbage-002":   "cl100k_base",
+}
+
+var modelToEncoding = map[string]Encoding{
+	// chat
+	"gpt-4":         "cl100k_base",
+	"gpt-3.5-turbo": "cl100k_base",
+	"gpt-35-turbo":  "cl100k_base", // Azure deployment name
+	// base
+	"davinci-002": "cl100k_base",
+	"babbage-002": "cl100k_base",
+	// embeddings
+	"text-embedding-ada-002": "cl100k_base",
+	// DEPRECATED MODELS
+	// text (DEPRECATED)
+	"text-davinci-003": "p50k_base",
+	"text-davinci-002": "p50k_base",
+	"text-davinci-001": "r50k_base",
+	"text-curie-001":   "r50k_base",
+	"text-babbage-001": "r50k_base",
+	"text-ada-001":     "r50k_base",
+	"davinci":          "r50k_base",
+	"curie":            "r50k_base",
+	"babbage":          "r50k_base",
+	"ada":              "r50k_base",
+	// code (DEPRECATED)
+	"code-davinci-002": "p50k_base",
+	"code-davinci-001": "p50k_base",
+	"code-cushman-002": "p50k_base",
+	"code-cushman-001": "p50k_base",
+	"davinci-codex":    "p50k_base",
+	"cushman-codex":    "p50k_base",
+	// edit (DEPRECATED)
+	"text-davinci-edit-001": "p50k_edit",
+	"code-davinci-edit-001": "p50k_edit",
+	// old embeddings (DEPRECATED)
+	"text-similarity-davinci-001":  "r50k_base",
+	"text-similarity-curie-001":    "r50k_base",
+	"text-similarity-babbage-001":  "r50k_base",
+	"text-similarity-ada-001":      "r50k_base",
+	"text-search-davinci-doc-001":  "r50k_base",
+	"text-search-curie-doc-001":    "r50k_base",
+	"text-search-babbage-doc-001":  "r50k_base",
+	"text-search-ada-doc-001":      "r50k_base",
+	"code-search-babbage-code-001": "r50k_base",
+	"code-search-ada-code-001":     "r50k_base",
+	// open source
+	"gpt2": "gpt2",
+}
+
+// EncodingForModel returns the encoding format for the specified OpenAI model.
+// If the specified model is not supported, an error is returned.
+func EncodingForModel(model string) (Encoding, error) {
+	if encoding, ok := modelToEncoding[model]; ok {
+		return encoding, nil
+	}
+	for prefix, encoding := range modelPrefixToEncoding {
+		if strings.HasPrefix(model, prefix) {
+			return encoding, nil
+		}
+	}
+	return "", ErrModelNotSupported
+}
+
 // ForModel returns a new instance of a Codec implementation based on the
 // specified OpenAI model. If the specified model is not supported, an error
 // is returned.
-func ForModel(model Model) (Codec, error) {
-	switch model {
-	case GPT4, GPT35Turbo, TextEmbeddingAda002:
-		return Get(Cl100kBase)
-
-	case TextDavinci003, TextDavinci002, CodeDavinci001,
-		CodeDavinci002, CodeCushman002, CodeCushman001,
-		DavinciCodex, CushmanCodex:
-		return Get(P50kBase)
-
-	case TextDavinci001, TextCurie001, TextBabbage001, TextAda001, Davinci,
-		Curie, Babbage, Ada, TextSimilarityDavinci001, TextSimilarityCurie001,
-		TextSimilarityBabbage001, TextSimilarityAda001, TextSearchDavinciDoc001,
-		TextSearchCurieDoc001, TextSearchAdaDoc001, TextSearchBabbageDoc001,
-		CodeSearchBabbageCode001, CodeSearchAdaCode001:
-		return Get(R50kBase)
-
-	case TextDavinciEdit001, CodeDavinciEdit001:
-		return Get(P50kEdit)
-
-	case GPT2:
-		return Get(GPT2Enc)
-	default:
-		return nil, ErrModelNotSupported
+func ForModel(model string) (Codec, error) {
+	enc, err := EncodingForModel(model)
+	if err != nil {
+		return nil, err
 	}
+	return Get(enc)
 }
